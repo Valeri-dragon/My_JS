4658f6f9acf003a55efb58d29446f944c5f9e353
Valeriya Terebilova <yakontekst01@yandex.ru>
2023-01-26 21:02:48 +0300

    the first task has been completed

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-const title = 'title';
-const screens = 'screens';
-const screenPrice = 0; 
+const title = "title";
+const screens = "screens";
+const screenPrice = 0;
 const rollback = "rollback";
- const fullPrice = "fullPrice"; 
-const  adaptive = "adaptive";
+const fullPrice = "fullPrice";
+const adaptive = "adaptive";
+console.log("lesson01");
+alert("Lesson01 - git");
