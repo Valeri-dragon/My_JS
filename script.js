40a9b10d98451f1b0c6616d3ac87ac8fea85b9c4
Valeriya Terebilova <yakontekst01@yandex.ru>
2023-01-26 17:23:53 +0300

    refactoring fucntion logged in script.js

--- script.js
+++ script.js
@@ -1,22 +1,4 @@
-"use strict";
-
-window.addEventListener("DOMContentLoaded", () => {
-  const myFirstvar = "1!!!2235";
-  const myFunc = () => {
-    console.log(myFirstvar);
-  };
-  /*try {
-    const body = document.querySelector("body");
-    document.addEventListener("click", (event) => {
-      event.preventDefault();
-      console.log("click");
-    });
-  } catch (error) {
-    console.error(error);
-  }*/
-  myFunc();
- 
-  [1,2,3].push();
-  console.log(myFirstvar);
-   alert("Hello world");
-});
+const div = document.getElementById("gitHub");
+const logged = ()=> {
+  console.log(div.textContent);
+}
