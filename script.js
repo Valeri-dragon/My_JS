65691252d28f2bb2c41094b1d5bcf501fec11cac
Valeriya Terebilova <yakontekst01@yandex.ru>
2023-01-26 20:59:58 +0300

    creating variables

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("hello world");
+const title = 'title';
+const screens = 'screens';
+const screenPrice = 0; 
+const rollback = "rollback";
+ const fullPrice = "fullPrice"; 
+const  adaptive = "adaptive";
