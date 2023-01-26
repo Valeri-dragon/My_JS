3a510bac31cb3b01e92c36c1d2a1b0d87a8cc777
Valeriya Terebilova <yakontekst01@yandex.ru>
2023-01-26 17:02:53 +0300

    add block menu in file index.html

---
+++ script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", () => {
+  const myFirstvar = "1!!!2235";
+  const myFunc = () => {
+    console.log(myFirstvar);
+  };
+  /*try {
+    const body = document.querySelector("body");
+    document.addEventListener("click", (event) => {
+      event.preventDefault();
+      console.log("click");
+    });
+  } catch (error) {
+    console.error(error);
+  }*/
+  myFunc();
+ 
+  [1,2,3].push();
+  console.log(myFirstvar);
+   alert("Hello world");
+});
